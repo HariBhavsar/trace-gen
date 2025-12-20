dd526cbfcf8d601488fc1f411fc0834830a36ad2
Hari <22b0934@iitb.ac.in>
2025-12-20 10:35:16 +0000

    [Infrastructure] Generated traces for other graphs

--- run.sh
+++ run.sh
@@ -23,7 +23,45 @@
 # python3 generate-bbv.py pr road 
 # python3 generate-bbv.py pr urand 
 
-python3 get-trace-from-simpoint.py pr web &
-python3 get-trace-from-simpoint.py pr kron &
-python3 get-trace-from-simpoint.py pr urand &
-python3 get-trace-from-simpoint.py pr road &
+# python3 get-trace-from-simpoint.py pr web &
+# python3 get-trace-from-simpoint.py pr kron &
+# python3 get-trace-from-simpoint.py pr urand &
+# python3 get-trace-from-simpoint.py pr road &
+
+# mkdir -p data/pr-web/
+# mkdir -p data/pr-kron/
+# mkdir -p data/pr-urand/
+# mkdir -p data/pr-road/
+
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-web-2478.trace.xz > data/pr-web/log_summary_Non-Inclusive_pr-web-2478.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-web-2478.trace.xz > data/pr-web/log_summary_Exclusive_pr-web-2478.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-web-2478.trace.xz > data/pr-web/log_summary_Re-Exclusive_pr-web-2478.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-web-2886.trace.xz > data/pr-web/log_summary_Non-Inclusive_pr-web-2886.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-web-2886.trace.xz > data/pr-web/log_summary_Exclusive_pr-web-2886.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-web-2886.trace.xz > data/pr-web/log_summary_Re-Exclusive_pr-web-2886.out &
+
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-kron-1656.trace.xz > data/pr-kron/log_summary_Non-Inclusive_pr-kron-1656.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-kron-1656.trace.xz > data/pr-kron/log_summary_Exclusive_pr-kron-1656.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-kron-1656.trace.xz > data/pr-kron/log_summary_Re-Exclusive_pr-kron-1656.out &
+
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-urand-183.trace.xz > data/pr-urand/log_summary_Non-Inclusive_pr-urand-183.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-urand-183.trace.xz > data/pr-urand/log_summary_Exclusive_pr-urand-183.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-urand-183.trace.xz > data/pr-urand/log_summary_Re-Exclusive_pr-urand-183.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-urand-468.trace.xz > data/pr-urand/log_summary_Non-Inclusive_pr-urand-468.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-urand-468.trace.xz > data/pr-urand/log_summary_Exclusive_pr-urand-468.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-urand-468.trace.xz > data/pr-urand/log_summary_Re-Exclusive_pr-urand-468.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-urand-1557.trace.xz > data/pr-urand/log_summary_Non-Inclusive_pr-urand-1557.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-urand-1557.trace.xz > data/pr-urand/log_summary_Exclusive_pr-urand-1557.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-urand-1557.trace.xz > data/pr-urand/log_summary_Re-Exclusive_pr-urand-1557.out &
+
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-road-261.trace.xz > data/pr-road/log_summary_Non-Inclusive_pr-road-261.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-road-261.trace.xz > data/pr-road/log_summary_Exclusive_pr-road-261.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-road-261.trace.xz > data/pr-road/log_summary_Re-Exclusive_pr-road-261.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-road-321.trace.xz > data/pr-road/log_summary_Non-Inclusive_pr-road-321.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-road-321.trace.xz > data/pr-road/log_summary_Exclusive_pr-road-321.out &
+# ../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-road-321.trace.xz > data/pr-road/log_summary_Re-Exclusive_pr-road-321.out &
+
+mkdir -p data/pr-uk02/
+../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Non-Inclusive -w 40000000 -i 100000000 ./pr-uk02-513.trace.xz > data/pr-uk02/log_summary_Non-Inclusive_pr-uk02-513.out &
+../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Exclusive -w 40000000 -i 100000000 ./pr-uk02-513.trace.xz > data/pr-uk02/log_summary_Exclusive_pr-uk02-513.out &
+../ReExclusive-refurbished/Champsims/BaseChampSim-Re-Exclusive/bin/Re-Exclusive -w 40000000 -i 100000000 ./pr-uk02-513.trace.xz > data/pr-uk02/log_summary_Re-Exclusive_pr-uk02-513.out &
